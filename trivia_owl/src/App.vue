f84ec9d709df0c7a2b35dd87c29ea8c768c605ed
lfernandes00 <luis_santos_fernandes@hotmail.com>
2020-12-20 16:51:03 +0000

    .

--- trivia_owl/src/App.vue
+++ trivia_owl/src/App.vue
@@ -1,27 +1,28 @@
 <template>
   <div id="app">
-    <NavBar></NavBar>
+      <NavBar></NavBar>
     <router-view />
   </div>
 </template>
 
 <script>
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
 export default {
-  name: 'app',
+  name: "app",
   components: {
     NavBar,
-  }
-}
+  },
+};
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: comfortaa, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  
 }
 
 #nav {
--- trivia_owl/src/components/NavBar.vue
+++ trivia_owl/src/components/NavBar.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="navbar">
-    <router-link :to="{ name: 'Home' }">Home</router-link> |
+    <!-- <router-link :to="{ name: 'Home' }">Home</router-link> |
     <router-link :to="{ name: 'Activities' }">Actividades</router-link> |
     <router-link :to="{ name: 'Classification' }">Classificação</router-link> |
     <router-link :to="{ name: 'Interaction' }">Interação</router-link> |
-    <div>
       <b-dropdown id="dropdown" text="Dropdown" class="m-md-2">
         <b-dropdown-item><router-link :to="{ name: 'Profile' }">Perfil</router-link></b-dropdown-item>
         <b-dropdown-item>Notificações(modal)</b-dropdown-item>
@@ -12,8 +11,32 @@
         <b-dropdown-item><router-link :to="{ name: 'ActivityManagement' }">Gerir Atividades</router-link></b-dropdown-item>
         <b-dropdown-item><router-link :to="{ name: 'Team' }">Equipa</router-link></b-dropdown-item>
         <b-dropdown-item>Login (modal)</b-dropdown-item>
-      </b-dropdown>
-    </div>
+      </b-dropdown> -->
+
+    <b-navbar toggleable="lg" style="height: 60px">
+      <router-link :to="{ name: 'Home' }"><img src="" alt=""></router-link>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <!--  class="mt-2 mr-4"-->
+        <b-navbar-nav class="ml-auto mr-2">
+          <router-link id="firstNav"  :to="{ name: 'Activities' }"><p class="mt-3">Atividades</p></router-link>
+          <router-link id='secondNav'  :to="{ name: 'Classification' }"><p class="mt-3">Classificação</p></router-link>
+          <router-link id="thirdNav"  :to="{ name: 'Interaction' }"><p class="mt-3">Interação</p></router-link>
+              
+          <b-nav-item-dropdown right class="mr-2 mt-2">
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em><img src="" alt=""></em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </div>
 </template>
 
@@ -24,4 +47,57 @@ export default {
 </script>
 
 <style>
+#navbar {
+  background-color: #1c2541;
+}
+
+#firstNav {
+  background-color: #1c2541;
+  border-block-color: #1c2541;
+  border-left-color: #1c2541;
+  height: 60px;
+  width: 150px;
+  color: #6FFFE9;
+}
+
+#firstNav:hover {
+  background-color: #6FFFE9;
+  color: #1c2541;
+}
+
+#secondNav {
+  background-color: #1c2541;
+  border-block-color: #1c2541;
+  border-left-color: #1c2541;
+  height: 60px;
+  width: 150px;
+  color: #6FFFE9;
+}
+
+#secondNav:hover {
+  background-color: #6FFFE9;
+  color: #1c2541;
+}
+
+#thirdNav {
+  background-color: #1c2541;
+  border-block-color: #1c2541;
+  border-left-color: #1c2541;
+  height: 60px;
+  width: 150px;
+  color: #6FFFE9;
+}
+
+#thirdNav:hover {
+  background-color: #6FFFE9;
+  color: #1c2541;
+}
+
+/* p {
+  color: #6FFFE9;
+}
+
+p:hover {
+color: #1c2541;
+} */
 </style>
